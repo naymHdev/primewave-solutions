be1f3beb7d75c8ff3d3cab20a342bdcb936709c6
MacBook Pro <naym100m@gmail.com>
2024-10-07 22:17:07 +0600

    [created] a primewave sitebar nav menus

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -2,6 +2,15 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 import { CiMenuFries } from "react-icons/ci";
+import { AiOutlineClose } from "react-icons/ai";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaWhatsapp,
+} from "react-icons/fa";
+import Image from "next/image";
+import logo from "../../../../public/images/PrimeWave-logo.png";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,45 +21,113 @@ const Navbar = () => {
 
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 bg-transparent">
-      <div className="container mx-auto flex items-center justify-between py-4 px-6">
+      <div className="container mx-auto flex items-center justify-between py-4 px-4 md:px-0">
         {/* Logo */}
         <div className="text-4xl font-extrabold">
           <span className=" text-black">PrimeWave</span>
         </div>
 
-        {/* Contact Us Button */}
-        <div className="block">
-          <motion.button
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-            className="bg-pink-500 text-white font-semibold py-2 px-6 rounded-lg transition-all"
-          >
-            Contact Us
-          </motion.button>
-        </div>
-
         {/* Hamburger Menu Icon */}
         <div className="">
           <button onClick={handleMenuToggle}>
-            <CiMenuFries className="h-8 w-8 text-pink-500" />
+            <CiMenuFries className="h-8 w-8 text-[#10355D]" />
           </button>
         </div>
       </div>
 
-      {/* Mobile Menu */}
-      {isOpen && (
-        <motion.div
-          initial={{ height: 0 }}
-          animate={{ height: "auto" }}
-          className="md:hidden bg-white shadow-md"
-        >
-          <div className="flex flex-col items-center py-4">
-            <a href="#contact" className="py-2 text-pink-500">
-              Contact Us
-            </a>
-          </div>
-        </motion.div>
-      )}
+      {/* Slide-in Mobile Menu */}
+      <motion.div
+        initial={{ x: "100%" }}
+        animate={{ x: isOpen ? 0 : "100%" }}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+        className={`fixed top-0 right-0 h-full md:w-[30%] w-full bg-[#10355D] text-white shadow-lg z-50 ${
+          isOpen ? "block" : "hidden"
+        }`}
+      >
+        {/* Close Button */}
+        <div className="flex justify-end p-4">
+          <button onClick={handleMenuToggle}>
+            <AiOutlineClose className="h-8 w-8 font-bold hover:text-[#104F89]" />
+          </button>
+        </div>
+
+        {/* Menu Items */}
+        <div className="flex flex-col items-end space-y-3 px-10">
+          <a
+            href="#home"
+            className="py-2 text-3xl font-bold hover:text-[#104F89]"
+          >
+            Home
+          </a>
+          <a
+            href="#about"
+            className="py-2 text-3xl font-bold hover:text-[#104F89]"
+          >
+            About
+          </a>
+          <a
+            href="#services"
+            className="py-2 text-3xl font-bold hover:text-[#104F89]"
+          >
+            Services
+          </a>
+          <a
+            href="#portfolio"
+            className="py-2 text-3xl font-bold hover:text-[#104F89]"
+          >
+            Portfolio
+          </a>
+          <a
+            href="#contact"
+            className="py-2 text-3xl font-bold hover:text-[#104F89]"
+          >
+            Contact Us
+          </a>
+        </div>
+
+        {/* Social media links with icons */}
+        <div className="flex items-center justify-end space-x-5 px-10 mt-6">
+          <a
+            className="hover:-mt-10 transition-all duration-300 rounded-full p-2 md:p-3 bg-[#1267b6]"
+            href="https://web.facebook.com/naymHdev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaFacebook className="text-xl" />
+          </a>
+          <a
+            className="hover:-mt-10 transition-all duration-300 rounded-full p-2 md:p-3 bg-[#1267b6]"
+            href="https://www.linkedin.com/in/naymhdev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram className="text-xl" />
+          </a>
+          <a
+            className="hover:-mt-10 transition-all duration-300 rounded-full p-2 md:p-3 bg-[#1267b6]"
+            href="https://www.linkedin.com/in/naymhdev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin className="text-xl" />
+          </a>
+          <a
+            className="hover:-mt-10 transition-all duration-300 rounded-full p-2 md:p-3 bg-[#1267b6]"
+            href="https://wa.me/8801770064053"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaWhatsapp className="text-xl" />
+          </a>
+        </div>
+        <div className="mt-10 text-end px-10">
+          <h2 className="text-5xl font-extrabold">PrimeWave</h2>
+          <div className="border border-[#1268B6] my-4" />
+          <p className=" font-medium text-foreground">
+            {/* hello.primewave@gmail.com */}
+          </p>
+        </div>
+      </motion.div>
     </nav>
   );
 };
