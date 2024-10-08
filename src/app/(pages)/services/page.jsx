536ca06fe6859302f4a5b6b2ebae55fcc2401ce5
MacBook Pro <naym100m@gmail.com>
2024-10-08 23:40:46 +0600

    [created] a main services section

--- src/app/(pages)/services/page.jsx
+++ src/app/(pages)/services/page.jsx
@@ -1,11 +1,13 @@
-
+import MainServices from "@/components/Pages/OurService/MainServices";
 
 const Services = () => {
-    return (
-        <>
-            <div></div>
-        </>
-    );
+  return (
+    <>
+      <div className="container mx-auto px-4 md:px-0 mt-24">
+        <MainServices />
+      </div>
+    </>
+  );
 };
 
 export default Services;
--- src/components/Pages/OurService/MainServices.jsx
+++ src/components/Pages/OurService/MainServices.jsx
@@ -1,13 +1,21 @@
+"use client";
+
 import { services } from "@/data/services";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 const MainServices = () => {
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-16">
         {services?.map((service, index) => (
-          <div key={index} className="border px-5 py-4 border-secondary">
-            <div>
+          <motion.div
+            key={index}
+            className="relative border p-8 border-secondary overflow-hidden"
+            whileHover={{ scale: 1.05 }}
+            transition={{ type: "spring", stiffness: 200 }}
+          >
+            <div className="flex flex-col items-center justify-center">
               <Image
                 src={service.image}
                 alt={service.description}
@@ -16,16 +24,39 @@ const MainServices = () => {
                 priority={true}
                 quality={100}
               />
+              <div className="mt-6">
+                <h2 className="text-2xl font-bold text-secondary">
+                  {service.category}
+                </h2>
+                <p className="mt-2 font-medium text-foreground">
+                  {service.description}
+                </p>
+              </div>
             </div>
-            <div className="mt-6">
-              <h2 className="text-2xl font-bold text-secondary">
-                {service.category}
-              </h2>
-              <p className="mt-2 font-medium text-foreground">
-                {service.description}
-              </p>
-            </div>
-          </div>
+
+            {/* Feature overlay */}
+            <motion.div
+              className="absolute inset-0 bg-secondary bg-opacity-100 p-8 text-white opacity-0 transition-all duration-300"
+              whileHover={{ opacity: 1 }}
+              transition={{ duration: 0.3 }}
+            >
+              <h3 className="font-bold text-3xl">{service.category}</h3>
+              <ul className="space-y-2 mt-6">
+                {service.features.map((feature, idx) => (
+                  <>
+                    <hr />
+                    <li key={idx} className="font-medium">
+                      {feature}
+                    </li>
+                  </>
+                ))}
+                <hr />
+              </ul>
+              <button className="border border-white px-5 font-medium py-2 mt-8">
+                Learn More
+              </button>
+            </motion.div>
+          </motion.div>
         ))}
       </div>
     </>
