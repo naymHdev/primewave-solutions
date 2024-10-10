a0261ffba2c74dc411bf7291f3f480d86b8ff681
MacBook Pro <naym100m@gmail.com>
2024-10-10 17:05:52 +0600

    [compleate] footer section

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -7,18 +7,21 @@ import {
   FaFacebook,
   FaInstagram,
   FaLinkedin,
+  FaRegCopyright,
   FaWhatsapp,
 } from "react-icons/fa";
+import { MdOutlineArrowOutward } from "react-icons/md";
+import "./footer.css";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
     <>
-      <div className=" bg-[#121212] py-20 mt-">
+      <div className=" bg-[#121212] mt-20">
         <ResponsiveContainer>
-          <footer className=" grid grid-cols-1 md:grid-cols-7">
-            <div className=" col-span-full md:col-span-5">
+          <footer className=" grid grid-cols-1 md:grid-cols-7 border-l border-l-[#1D1D1D] pl-1">
+            <div className=" col-span-full md:col-span-5 py-20">
               <div className=" grid grid-cols-1 md:grid-cols-2 gap-4">
                 <div>
                   <h2 className=" text-3xl font-black text-legendary">
@@ -33,7 +36,7 @@ const Footer = () => {
                   </p>
 
                   {/* Social media icons */}
-                  <div className="flex items-center space-x-6 py-8">
+                  <div className="flex items-center space-x-6 py-12">
                     <a
                       className="hover:-mt-10 transition-all duration-300"
                       href="https://web.facebook.com/naymHdev"
@@ -75,7 +78,7 @@ const Footer = () => {
                       Based in the USA, offering global services and working
                       remotely. Reach out to us!
                     </h3>
-                    <div className=" mt-10 space-y-2">
+                    <div className=" mt-10 space-y-1">
                       <p>
                         <a
                           href="https://wa.me/88001770064053"
@@ -99,32 +102,57 @@ const Footer = () => {
                 </div>
               </div>
             </div>
-            <div className="bg-[#1D1D1D] col-span-full md:col-span-2"></div>
+            <div className="bg-[#1D1D1D] col-span-full md:col-span-2 py-20">
+              <h2 className=" text-xl font-black text-white text-center mb-8">
+                Got a project?
+              </h2>
+
+              <div className="flex flex-col items-center justify-center">
+                <Link href="#">
+                  <div className="wave-button text-white font-medium">
+                    <span>
+                      Let's Talk <MdOutlineArrowOutward size={20} />
+                    </span>
+                    <div class="wave"></div>
+                  </div>
+                </Link>
+              </div>
+              <div className=" text-center font-black mt-8">
+                <h2 className=" text-white text-xl">09:00 AM - 22:00 PM</h2>
+                <p className=" text-gray-400 mt-1">Everyday</p>
+              </div>
+            </div>
           </footer>
 
           {/* Bottom Footer Section */}
-          <footer className="border border-[#1D1D1D] py-6 px-1 grid grid-cols-1 md:grid-cols-2 items-center">
-            <div className=" flex items-center gap-2 text-white text-lg font-semibold">
-              <h3 className="">
-                PrimeWave <span>{currentYear}</span>
-              </h3>
-              <div>/</div>
-              <Link href="#" className=" underline hover:text-legendary">
+          <footer className="border border-[#1D1D1D] py-6 px-1 grid grid-cols-1 md:grid-cols-3 items-center space-y-5 md:space-y-0">
+            <div className="text-white font-medium md:font-semibold flex space-x-4 items-center">
+              <Link href="#" className="hover:text-legendary">
                 Privacy Policy
               </Link>
+              <Link href="#" className="hover:text-legendary">
+                Terms
+              </Link>
             </div>
-            <div className=" flex items-center relative">
-              <div className=" rounded-full p-1 bg-white text-black absolute -left-3">
+
+            <div className=" flex items-center justify-start md:justify-center relative">
+              <div className=" rounded-full p-1 bg-white text-black absolute -left-3 md:left-[68px]">
                 <BsShieldLockFill />
               </div>
-              <div className="bg-[#A3A3A3] text-white pl-3 px-1 font-semibold">
+              <div className="bg-[#A3A3A3] text-white pl-3 px-1 font-medium md:font-semibold">
                 DMC
               </div>
-              <div className=" bg-[#272727] text-white px-2 font-semibold">
+              <div className=" bg-[#272727] text-white px-2 font-medium md:font-semibold">
                 PROTECTED
               </div>
             </div>
-            <div></div>
+            <div className=" flex items-center justify-start md:justify-end gap-2 text-white text-lg font-medium md:font-semibold">
+              <h3>PrimeWave</h3>
+              <h2>
+                <FaRegCopyright />
+              </h2>
+              <h2>{currentYear}</h2>
+            </div>
           </footer>
         </ResponsiveContainer>
       </div>
