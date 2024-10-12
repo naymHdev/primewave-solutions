4647d0f65101ac8bca0e2baf1ea12a088c8ca99b
MacBook Pro <naym100m@gmail.com>
2024-10-12 17:47:15 +0600

    [created a] a image gallery with carosule

--- src/components/Pages/Portfolio/Gallery.jsx
+++ src/components/Pages/Portfolio/Gallery.jsx
@@ -1,7 +1,53 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./gallery.css";
+
 import ResponsiveContainer from "@/components/common/ResponsiveContainer";
 import SectionName from "@/components/common/SectionName";
+import { imageGallery } from "@/data/galleryData";
 
 const Gallery = () => {
+  const settings = {
+    dots: false,
+    fade: true,
+    infinite: true,
+    speed: 1400,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <>
       <SectionName
@@ -10,7 +56,23 @@ const Gallery = () => {
         details="See our work in action through an interactive gallery of websites, apps, and digital experiences. Explore how each project was crafted with care to deliver stunning results."
       />
       <div>
-        <ResponsiveContainer></ResponsiveContainer>
+        <ResponsiveContainer>
+          <div className="slider-container">
+            <Slider {...settings}>
+              {imageGallery?.map((img) => (
+                <div key={img.id}>
+                  <div className="relative h-full ">
+                    <img
+                      className="w-full h-full object-cover object-center"
+                      src={img.image}
+                      alt={img.id}
+                    />
+                  </div>
+                </div>
+              ))}
+            </Slider>
+          </div>
+        </ResponsiveContainer>
       </div>
     </>
   );
---
+++ src/data/galleryData.js
@@ -0,0 +1,32 @@
+export const imageGallery = [
+  {
+    id: 1,
+    image: "/images/work-baner1.png",
+    title: "",
+    subTitle: "",
+  },
+  {
+    id: 2,
+    image: "/images/work-banner2.png",
+    title: "",
+    subTitle: "",
+  },
+  {
+    id: 3,
+    image: "/images/work-banner3.png",
+    title: "",
+    subTitle: "",
+  },
+  {
+    id: 4,
+    image: "/images/work-banner4.png",
+    title: "",
+    subTitle: "",
+  },
+  {
+    id: 5,
+    image: "/images/work-banner5.png",
+    title: "",
+    subTitle: "",
+  },
+];
